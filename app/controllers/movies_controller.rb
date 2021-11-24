899192f2e67ba4fe9711ba4f619ab513ec8ca8d4
Philip <philipmjobe@gmail.com>
2021-11-24 14:12:28 -0500

    added show and index logic to movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+  def index 
+    movie = Movie.available
+    render json: movie
+  end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+    render json: movie
+  end 
 end
