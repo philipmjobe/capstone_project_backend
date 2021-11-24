58ea0c71a5b3f0bb18895ef8e033e8fd3aff6890
Philip <philipmjobe@gmail.com>
2021-11-24 14:50:28 -0500

    added watch_lists controller and I think the correct logic to add a movie to the list and to check it off when you have watched it.

---
+++ app/controllers/watch_lists_controller.rb
@@ -0,0 +1,25 @@
+class WatchListsController < ApplicationController
+  def create
+    movie = Watch_list.new(
+      name: params[:name],
+      description: params[:description],
+      box_art: params[:box_art],
+      sub_genre: params[:sub_genre]
+    )
+    if movie.save
+      render json: movie
+    else
+      render json: {errors: movie.errors.full_messages}, status: 406
+    end
+  end 
+
+  def update
+    movie = Movie.find_by(id: params[:id])
+    if movie == "watched"
+      watched = true
+    else
+      watched = false
+    end 
+  end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   ### USERS ROUTES ###
   post "/users" => "users#create"
+  
   ### SESSIONS ROUTES ###
   post "/sessions" => "sessions#create"
+  
   ### MOVIES ROUTES ###
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
+
+  ### WATCH_LISTS ROUTES
+  post "/watch_lists" => "watch_lists#create"
+  patch "/watch_lists" => "watch_lists#update"
 end
