53a9314290c0dd4fee2da08b90f4b23c138c7b68
Philip <philipmjobe@gmail.com>
2022-01-10 14:20:02 -0500

    added loveit controller and model

---
+++ app/controllers/loveits_controller.rb
@@ -0,0 +1,2 @@
+class LoveitsController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_10_190729) do
+ActiveRecord::Schema.define(version: 2022_01_10_191339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,6 @@ ActiveRecord::Schema.define(version: 2022_01_10_190729) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
-  end
-
-  create_table "lovedits", force: :cascade do |t|
-    t.integer "movie_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "loveits", force: :cascade do |t|
