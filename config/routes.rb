689a1892959ae3c82f37e85352ba2b8dc1240a8d
Philip <philipmjobe@gmail.com>
2022-01-14 15:11:12 -0500

    added a route to delete movies from the list now just have to link it up on the frontend

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get "/lists" => "lists#index"
   post "/lists" => "lists#create"
   patch "/lists/:id" => "lists#update"
-  delete "/lists/:id" => "list#destroy"
+  delete "/lists/:id" => "lists#destroy"
 
   ### LOVEDIT ROUTES
   get "/lovedits" => "lovedits#index"
