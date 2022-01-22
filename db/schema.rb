092664eb71fd4caec5780518d7aa8a5769473d4b
Philip <philipmjobe@gmail.com>
2022-01-22 15:05:34 -0500

    removed category from the lists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_214244) do
+ActiveRecord::Schema.define(version: 2022_01_22_200328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2022_01_12_214244) do
     t.boolean "watched"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "category"
   end
 
   create_table "lovedits", force: :cascade do |t|
