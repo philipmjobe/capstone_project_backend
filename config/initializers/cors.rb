d44d6827b2861eadb88b957cba2a12a569909602
Philip <philipmjobe@gmail.com>
2022-03-09 14:59:43 -0500

    returns cors.rb to heroku link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins 'https://git.heroku.com/horrormoviebucketlist.git'
 
     resource '*',
       headers: :any,
