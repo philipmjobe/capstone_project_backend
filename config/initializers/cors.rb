6b0a9c03e9b5a1523f9f54bc6364bfb3eae19f8b
Philip <philipmjobe@gmail.com>
2022-03-09 13:34:10 -0500

    fixes cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://git.heroku.com/horrormoviebucketlist.git'
 
     resource '*',
       headers: :any,
